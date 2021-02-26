fec69a9d7df29dc8a114b3e8898d8a0fd3d87446
Menamonmon <monyasser111@gmail.com>
2021-02-26 15:54:45 -0500

    added ranking sents

--- main.py
+++ main.py
@@ -41,15 +41,25 @@ def main():
 
 	# Lemmaization is the process of simplyfying the word so that it's in its most simple form
 	# ex: walking  =>  walk
-	token_lemmas = [token.lemma_.lower() for token in tokens]
-	freq_table = make_frequency_table(token_lemmas)
+	token_lemmas = [token.lemma_ for token in tokens]
+	word_scores_table = make_frequency_table(token_lemmas)
 	
 	# Sorting the table by the words that occur the most in the document
 	table_to_list = [(key, value) for key, value in freq_table.items()]
 	sorted_list = sorted(table_to_list, key=lambda x: x[1], reverse=True)
 
 	# Printing all of the words that occur more than once
-	[print(row) for row in sorted_list if row[1] > 1]
+	sents_rank = []
+	sent_len_min = 15
+	for sent in sentences:
+		if len(sent) < sent_len_min:
+			continue
+
+		sent_score = sum(word_scores_table.get(str(word.lemma_), 0) for word in sent) / len(sent)
+		sents_rank.append((sent, sent_score))
+
+	sents_sorted = sorted(sents_rank, key=lambda x: x[1], reverse=True)
+	[print(sent) for (sent, score) in sents_sorted[:10]]
 
 if __name__ == "__main__":
   main()
